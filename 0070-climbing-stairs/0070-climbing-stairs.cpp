b51f98823a7067f0e43077e4373bcbf0d9b3987e
yatharth2soni <soniyatharth141@gmail.com>
2026-07-11 16:11:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.54%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+       // const int MOD = 1e9 + 7;
+       if(n==1){
+        return 1;
+       }
+
+        vector<int>dp(n+1);
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;i++){
+            dp[i]=(dp[i-1]+dp[i-2]);
+        }
+        return dp[n];
+    }
+};
